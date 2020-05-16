ed9b21f305cbb6a67292ed12ef6ea275a4399a19
Ollie Crook <ollie@hubblehq.com>
2020-05-16 16:40:43 +0100

    Create home page
    Use the Layout component to create a quick home page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,47 @@
-import React from "react"
+import React from "react";
 
-export default () => <div>Hello world!</div>
+import Layout from "../layout/Layout";
+
+export default () => (
+  <Layout>
+    <p>Hi. My name is Ollie.</p>
+    <p>I’m a software developer, noisy musician and cat enthusiast.</p>
+    <p>
+      I like building great front end applications with a passion for making
+      accessible web experiences.
+    </p>
+    <p>
+      <strong>oilyquack</strong> is a{" "}
+      <a
+        href="https://en.wikipedia.org/wiki/Malapropism"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        malapropism
+      </a>{" "}
+      of my real name. I started using it when I was a teacher to stop students
+      finding me online. I’m too fond of how silly it sounds to change it back.
+      I also get a kick out of people saying it out loud in job interviews.
+    </p>
+    <p>
+      You can see some of my work on{" "}
+      <a
+        href="https://github.com/oilyquack"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        GitHub
+      </a>{" "}
+      or read my ramblings on{" "}
+      <a
+        href="https://twitter.com/oilyquack"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Twitter
+      </a>
+      .
+    </p>
+    <img src="" />
+  </Layout>
+);
