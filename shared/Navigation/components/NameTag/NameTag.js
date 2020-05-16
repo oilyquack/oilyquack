615e6ed8023cab2a36fe56bb7ce79fa54a1e6d6e
Ollie Crook <ollie@hubblehq.com>
2020-05-16 16:37:22 +0100

    Create NameTag comp to use on Nav bar
    I'm far prouder of this than I probably should be. This little
    component with a scrolling name plate was basically the inspiration
    for starting this whole project.
    
    TODO:
    * Test
    * See if I can left align the word `oily` better so that it sits
    flush to th left gutter.

---
+++ shared/Navigation/components/NameTag/NameTag.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import styles from "./NameTag.module.css";
+
+const NameTag = () => (
+  <div className={styles.NameTag}>
+    <span className={styles.firstName}>
+      <span>Ollie</span>
+      <span>Oily </span>
+    </span>
+    <span> </span>
+    <span className={styles.lastName}>
+      <span>Crook</span>
+      <span>Quack</span>
+    </span>
+  </div>
+);
+export default NameTag;
