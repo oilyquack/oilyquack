deadaadcea1d1bda9380be56c256675eeff22af7
Ollie Crook <ollie@hubblehq.com>
2020-05-23 11:35:58 +0100

    Add SEO to 404 and Blog pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,9 +2,11 @@ import React from "react";
 
 import Layout from "../layout/Layout";
 import FourOhFour from "../../features/FourOhFour";
+import SEO from "../components/SEO";
 
 export default () => (
   <Layout>
+    <SEO />
     <FourOhFour />
   </Layout>
 );
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,9 +2,11 @@ import React from "react";
 
 import Layout from "../layout/Layout";
 import Blog from "../../features/Blog";
+import SEO from "../components/SEO";
 
 export default () => (
   <Layout>
+    <SEO />
     <Blog />
   </Layout>
 );
