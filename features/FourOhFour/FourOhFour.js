4b3e2171b2715c947bba90f953f7ac9be213c79d
Ollie Crook <ollie@hubblehq.com>
2020-06-01 16:40:49 +0100

    Finish 404 page
    The page now has content and actually links back to the Home page.
    In future, I won't need the link as at some point there'll be more
    pages in the currently unused Navigation component. Until then,
    we need to explicitly link back to the home page somehow.

--- features/FourOhFour/FourOhFour.js
+++ features/FourOhFour/FourOhFour.js
@@ -1,8 +1,31 @@
+import { Link } from "gatsby";
 import React from "react";
+
+import david from "./assets/davidbyrne.gif";
+import styles from "./FourOhFour.module.css";
 
 const FourOhFour = () => (
   <>
-    <h1>Uh oh!</h1>
+    <h1>FOUR, OH FOUR!</h1>
+    <p>
+      <Link to="/">Home</Link> is where you want to be.
+    </p>
+    <div className={styles.content}>
+      <img alt="David Byrne" className={styles.image} src={david} />
+
+      <span className={styles.quote}>
+        <p>And you may ask yourself</p>
+        <p>How do I work this?</p>
+        <p>And you may ask yourself</p>
+        <p>Where is that large automobile?</p>
+        <p>And you may tell yourself</p>
+        <p>This is not my beautiful house!</p>
+        <p>And you may tell yourself</p>
+        <p>This is not my beautiful wife!</p>
+
+        <p className={styles.author}>— David Byrne, 1980</p>
+      </span>
+    </div>
   </>
 );
 
