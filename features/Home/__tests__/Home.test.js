da8c3f01d8d5cbdc0e7cdcabc40effdeb015cf3a
Ollie Crook <ollie@hubblehq.com>
2020-07-29 12:53:08 +0100

    Improve Home test suite
    Test the copy and links on the Home page are behaving as expected.

--- features/Home/__tests__/Home.test.js
+++ features/Home/__tests__/Home.test.js
@@ -8,4 +8,34 @@ describe("features : Home", () => {
     const { container } = render(<Home />);
     expect(container).toMatchSnapshot();
   });
+
+  it("should link to malapropism definition on wikipedia", () => {
+    const { getByText } = render(<Home />);
+
+    const link = getByText(/malapropism/);
+
+    const resultHref = new URL(link.href).toString();
+
+    expect(resultHref).toBe("https://en.wikipedia.org/wiki/Malapropism");
+  });
+
+  it("should link to my GitHub page", () => {
+    const { getByText } = render(<Home />);
+
+    const link = getByText(/GitHub/);
+
+    const resultHref = new URL(link.href).toString();
+
+    expect(resultHref).toBe("https://github.com/oilyquack");
+  });
+
+  it("should link to my Twitter page", () => {
+    const { getByText } = render(<Home />);
+
+    const link = getByText(/Twitter/);
+
+    const resultHref = new URL(link.href).toString();
+
+    expect(resultHref).toBe("https://twitter.com/oilyquack");
+  });
 });
